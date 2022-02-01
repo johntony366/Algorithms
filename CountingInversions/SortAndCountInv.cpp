fd8f77243946d7eb11373f93f2ed6a1f897c0e53
John Tony <johntony366@gmail.com>
2022-02-01 17:32:45 +0530

    Fixed count inversions

--- CountingInversions/SortAndCountInv.cpp
+++ CountingInversions/SortAndCountInv.cpp
@@ -1,74 +1,58 @@
 #include <iostream>
-#include <algorithm>
-#include <vector>
 
-int mergeAndCountSplitInv(std::vector<int> a, int start, int mid, int end)
+int merge(long long int arr[], long long int temp[], long long int left, long long int mid, long long int right) 
 {
-    int countSplitInv = 0;
-    int n1 = mid - start + 1;
-    int n2 = end - mid;
-    std::vector<int> L(n1);
-    std::vector<int> R(n2);
-    for (int i = 0; i < n1; ++i)
-    {
-        L[i] = a[start + i];
-    }
-    for (int i = 0; i < n2; ++i)
-    {
-        R[i] = a[mid + 1 + i];
-    }
-    int i{0}, j{0}, k{start};
-    while (i < n1 && j < n2)
-    {
-        if (L[i] < R[j])
-        {
-            a[k] = L[i];
-            ++i;
-            ++k;
-        }
-        else
-        {
-            a[k] = R[j];
-            countSplitInv += n1 - i;
-            ++j;
-            ++k;
-        }
-    }
-    while (i < n1)
-    {
-        a[k] = L[i];
-        ++i;
-        ++k;
-    }
-
-    while (j < n2)
-    {
-        a[k] = L[j];
-        ++j;
-        ++k;
-    }
-
-    return countSplitInv;
+   long long int i, j, k;
+   long long int count = 0;
+   i = left; //i to locate first array location
+   j = mid; //i to locate second array location
+   k = left; //i to locate merged array location
+   while ((i <= mid - 1) && (j <= right)) 
+   {
+      if (arr[i] <= arr[j])
+      { //when left item is less than right item
+        temp[k++] = arr[i++];
+      } 
+      else 
+      {
+         temp[k++] = arr[j++];
+         count += (mid - i); //find how many convertion is performed
+      }
+   }
+   while (i <= mid - 1) //if first list has remaining item, add them in the list
+      temp[k++] = arr[i++];
+   while (j <= right) //if second list has remaining item, add them in the list
+      temp[k++] = arr[j++];
+   for (i=left; i <= right; i++)
+      arr[i] = temp[i]; //store temp Array to main array
+   return count;
 }
 
-int sortAndCountInv(std::vector<int> a, int start, int end)
+long long int mergeSort(long long int arr[], long long int temp[], long long int left, long long int right)
 {
-    int inv = 0;
-
-    if (start < end)
-    {
-        int mid = start + (end - start)/2;
-        inv += sortAndCountInv(a, start, mid);
-        inv += sortAndCountInv(a, mid+1, end);
-        inv += mergeAndCountSplitInv(a, start, mid, end);
-    }
-    return inv;
-
+   long long int mid, count = 0;
+   if (right > left) 
+   {
+      mid = (right + left)/2; //find mid index of the array
+      count = mergeSort(arr, temp, left, mid); //merge sort left sub array
+      count += mergeSort(arr, temp, mid+1, right); //merge sort right sub array
+      count += merge(arr, temp, left, mid+1, right); //merge two sub arrays
+   }
+   return count;
 }
 
-
-int main()
+long long int arrInversion(long long int arr[], long long int n)
 {
-    std::vector<int> a{ 1, 20, 6, 4, 5 };
-    std::cout << sortAndCountInv(a, 0, std::size(a)-1) << '\n';
+   long long int temp[n];
+   return mergeSort(arr, temp, 0, n - 1);
+}
+
+int main() 
+{
+   long long int arr[] = 
+   {
+       2, 4, 1, 3, 5
+    };
+   long long int n = 5;
+   std::cout << "The number of inversions is "<< arrInversion(arr, n) << '\n';
 }
