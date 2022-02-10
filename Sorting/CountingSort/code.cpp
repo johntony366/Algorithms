63671c4a91b423a48620bd013ddb16e30c13e128
John Tony <johntony366@gmail.com>
2022-02-10 12:27:18 +0530

    Updated counting sort, find k using for loop

--- Sorting/CountingSort/code.cpp
+++ Sorting/CountingSort/code.cpp
@@ -1,7 +1,16 @@
 #include <bits/stdc++.h>
 
-void countingSort(int* A, int n, int k, int* result)
+void countingSort(int* A, int n, int* result)
 {
+    //find k
+    int k = A[0];
+    for (int i = 1; i < n; ++i)
+    {
+        if (A[i] > k)
+        {
+            k = A[i];
+        }
+    }
 
     int temp[k+1]{ 0 };
 
@@ -26,12 +35,11 @@ int main()
 {
     int A[]{ 2, 5, 3, 0, 2, 3, 0, 3 };
     int n = std::size(A);
-    int k = 5;
 
     int result[n];
     std::copy(A, A + n, result);
 
-    countingSort(A, n, k, result);
+    countingSort(A, n, result);
 
     for (int i = 0; i < n; ++i)
     {
